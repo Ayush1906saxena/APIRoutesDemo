1b854ca4f100ddb1b7a12a61e3d75fea8389233f
Ayush Saxena <ayush.saxena2019@vitstudent.ac.in>
2024-03-18 06:49:05 +0530

    Writing my first POST request and storing data received

--- pages/index.js
+++ pages/index.js
@@ -1,15 +1,41 @@
+import { useRef } from "react";
 function HomePage() {
+  const emailInputRef = useRef();
+  const feedbackInputRef = useRef();
+
+  function submitFormHandler(event) {
+    event.preventDefault(); //preventing the default behavior to reload on submit
+
+    const enteredEmail = emailInputRef.current.value;
+    const enteredFeedback = feedbackInputRef.current.value;
+
+    const reqBody = {
+      email: enteredEmail,
+      text: enteredFeedback,
+    };
+
+    fetch("/api/feedback/", {
+      method: "POST",
+      body: JSON.stringify(reqBody),
+      headers: {
+        "Content-Type": "application/json",
+      },
+    })
+      .then((response) => response.json)
+      .then((data) => console.log(data));
+  }
+
   return (
     <div>
       <h1>The Home Page</h1>
-      <form>
+      <form onSubmit={submitFormHandler}>
         <div>
           <label htmlFor="email">Your Email Address</label>
-          <input type="email" id="email" />
+          <input type="email" id="email" ref={emailInputRef} />
         </div>
         <div>
           <label htmlFor="feedback">Your Feedback</label>
-          <textarea id="feedback" rows="5"></textarea>
+          <textarea id="feedback" rows="5" ref={feedbackInputRef}></textarea>
         </div>
         <button>Send Feedback</button>
       </form>
