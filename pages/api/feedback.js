7a3c61ce8c24fe9e881ef9bdbb0cd38f73bb3ff9
Ayush Saxena <ayush.saxena2019@vitstudent.ac.in>
2024-03-25 19:39:55 +0530

    Using a dynamic API route

--- pages/api/feedback.js
+++ pages/api/feedback.js
@@ -1,11 +1,12 @@
 import fs from "fs";
 import path from "path";
 
-function buildFeedbackPath() {
+// Adding export so that the functions can be called from other pages too
+export function buildFeedbackPath() {
   return path.join(process.cwd(), "data", "feedback.json");
 }
 
-function extractFeedback(filePath) {
+export function extractFeedback(filePath) {
   const fileData = fs.readFileSync(filePath);
   const data = JSON.parse(fileData); //string -> json object
   return data;
---
+++ pages/feedback/index.js
@@ -0,0 +1,46 @@
+import { buildFeedbackPath, extractFeedback } from "../api/feedback";
+import { useState } from "react";
+import { Fragment } from "react";
+export default function FeedbackPage(props) {
+  const [feedbackData, setFeedbackData] = useState();
+
+  function loadFeedbackHandler(id) {
+    fetch(`/api/${id}`)
+      .then((response) => response.json())
+      .then((data) => {
+        setFeedbackData(data.feedback);
+      });
+  }
+  return (
+    <Fragment>
+      {feedbackData && <p>{feedbackData.email}</p>}
+      <ul>
+        {props.feedbackItems.map((item) => (
+          <li key={item.id}>
+            {/* pre configuring the onClick handler function with id
+            first argument = the value of this property
+            second argument = the value of first parameter
+           */}
+            {item.text}{" "}
+            <button onClick={loadFeedbackHandler.bind(null, item.id)}>
+              Show Details
+            </button>
+          </li>
+        ))}
+      </ul>
+    </Fragment>
+  );
+}
+
+export async function getStaticProps() {
+  // You should not use fetch() for talking to your APIs,
+  // Do it only while talking to external APIs like Firebase
+
+  const filePath = buildFeedbackPath();
+  const data = extractFeedback(filePath);
+  return {
+    props: {
+      feedbackItems: data,
+    },
+  };
+}
